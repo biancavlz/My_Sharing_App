a5e454a3f59bd68c6d4f2e7d2aa986c785a49f7d
Bianca <ing_biank@hotmail.com>
2018-10-07 18:20:45 +0200

    Creates routes for posts and nested routes for photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show]
+  resources :posts, only: [:index, :show, :create] do
+    resources :photos, only: [:create]
+  end
 end
